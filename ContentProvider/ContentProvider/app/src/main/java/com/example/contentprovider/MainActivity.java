7be8acf10924fed1cfb67f18f34a6fdc4384d51e
truongduy020406 <votruongduya2@gmail.com>
2022-10-26 18:13:51 +0700

    contact

---
+++ ContentProvider/ContentProvider/app/src/main/java/com/example/contentprovider/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.contentprovider;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView main_rcv;
+    ContactAdapter contactAdapter;
+    ContentResolver contentResolver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        main_rcv = findViewById(R.id.main_rcv);
+        contactAdapter = new ContactAdapter(MainActivity.this);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 1);
+        main_rcv.setLayoutManager(gridLayoutManager);
+        contactAdapter.setData(getList());
+        main_rcv.setAdapter(contactAdapter);
+
+
+    }
+
+    @SuppressLint("Range")
+    public List<Contact> getList() {
+        List<Contact> list = new ArrayList<>();
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS}, 100);
+        }
+        contentResolver = getContentResolver();
+
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                new String[]{ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts._ID},
+                null, null, null);
+        while(cursor.moveToNext()) {
+            String phone = "";
+            long id = cursor.getLong(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            Cursor c = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{String.valueOf(id)}, null);
+
+            while(c.moveToNext()) {
+                phone += c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)) + "   ";
+
+            }
+            list.add(new Contact(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)), phone));
+        }
+
+
+        return list;
+    }
+
+}
